501c40dc1c728268cab0852d4ca984925e69000d
Leart Krasniqi <leart.krasniqi@prosperoware.com>
2022-01-09 04:59:24 +0100

    Created simple server

--- myApp.js
+++ myApp.js
@@ -2,7 +2,12 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 console.log("test");
-
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server listening");
+})
+app.get('/',(req,res) =>{
+    res.send("Response String");
+})
 
 
 
