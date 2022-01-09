8a8a647c1f2c88fcaa08b74976711fd2affd308f
Leart Krasniqi <leart.krasniqi@prosperoware.com>
2022-01-09 14:31:23 +0100

    Added /json route

--- myApp.js
+++ myApp.js
@@ -10,7 +10,9 @@ app.use('/public',express.static(path.join(__dirname,"/public")));
 app.get('/',(req,res) =>{
     res.sendFile(__dirname+"/views/index.html");
 })
-
+app.get('/json', (req,res)=> {
+    res.json({"message":"Hello json"});
+})
 
 
 
