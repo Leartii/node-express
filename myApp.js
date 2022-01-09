412bc98539407c11073608ae23c34683b3fd0dc8
Leart Krasniqi <leart.krasniqi@prosperoware.com>
2022-01-09 05:07:13 +0100

    Simple bug fix

--- myApp.js
+++ myApp.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 console.log("test");
-app.listen(process.env.PORT || 3000, function() {
+/*app.listen(process.env.PORT || 3000, function() {
     console.log("Server listening");
-})
+})*/
 app.get('/',(req,res) =>{
     res.send("Hello Express");
 })
