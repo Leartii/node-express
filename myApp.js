4dc061885282379f42d2ffe026f3377492583e47
Leart Krasniqi <leart.krasniqi@prosperoware.com>
2022-01-09 14:25:34 +0100

    Added static resources

--- myApp.js
+++ myApp.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var path = require("path");
 var app = express();
 console.log("Hello World");
 console.log("test");
 /*app.listen(process.env.PORT || 3000, function() {
     console.log("Server listening");
 })*/
+app.use('/public',express.static(path.join(__dirname,"/public")));
 app.get('/',(req,res) =>{
     res.sendFile(__dirname+"/views/index.html");
 })
